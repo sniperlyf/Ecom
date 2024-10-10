c72e8ae62dbb65f67f2f17830371d172939d116c
krishna <109722847+sniperlyf@users.noreply.github.com>
2024-10-10 14:33:03 +0530

    Add files via upload

---
+++ Shopping Site Project FEE-3/components/footer1.js
@@ -0,0 +1,84 @@
+let exportF1 = () => {
+    return `<hr id="divider_footer">
+    <section id="strip_one">
+    <h2>Why buy from geekbuying?</h2>
+    <h2>+</h2>
+</section>
+<hr id="divider_footer">
+<section id="strip_two">
+    <div id="search_box">
+        <input type="text" placeholder="Enter email to get a 5% off coupon">
+        <button>Subscribe</button>
+    </div>
+</section>
+<hr id="divider_footer">
+<section id="strip_three">
+    <sub class="top">
+        <div>
+            <h3>Help</h3>
+            <br>
+            <p>Order Status</p>
+            <br>
+            <p>About us</p>
+            <br>
+            <p>Sitemap</p>
+        </div>
+        <div>
+            <h3>Support</h3>
+            <br>
+            <p>Payment information</p>
+            <br>
+            <p>Shipping guide</p>
+            <br>
+            <p>Wholesale</p>
+        </div>
+        <div>
+            <h3>Policies</h3>
+            <br>
+            <p>Terms & conditions</p>
+            <br>
+            <p>Return policy</p>
+            <br>
+        </div>
+        <div>
+            <h3>Download the app</h3>
+            <br>
+            <img src="./images/App_stores_and_scan.png" alt="Download the App">
+        </div>
+        <div>
+            <span id="other_options">
+                <div>
+                    <img src="./images/email_logo.png" alt="email">
+                </div>
+                <div>
+                    <p>Service_payment@geekbuying.com</p>
+                </div>
+            </span>
+            <span id="other_options">
+                <div>
+                    <img src="./images/ticket_logo.png" alt="ticket">
+                </div>
+                <div>
+                    <p>Ticket</p>
+                </div>
+            </span>
+            <span id="other_options">
+                <div>
+                    <img src="./images/chat_logo.png" alt="chat">
+                </div>
+                <div>
+                    <p>Live Chat</p>
+                </div>
+            </span>
+        </div>
+    </sub>
+    <sub class="middle">
+        
+        <div class="languages">
+            <p>English | Español | Français| Deutsch | Italiano | Português | Nederlands | Polski | Русский | Türkçe | ελληνικά | Magyar | لعربية | עברית| ไทย | 日本語 |</p>
+        </div>
+        <hr id="divider_footer">
+    </sub>
+</section>`;
+}
+export default exportF1;
---
+++ Shopping Site Project FEE-3/components/footer2.js
@@ -0,0 +1,11 @@
+let exportF2 = () => {
+    return `<section id="strip_four">
+    <img src="./images/Bottom_banners.png" alt="Bottom Banners">
+</section>
+<hr id="divider_footer">
+<section id="strip_five">
+    <p>Copyright © 2012-2024 GeekBuying.com. G-24(Group-11) All rights reserved.</p>
+    <p>2210991809 - krishna || 2210991802 - Khushvir Singh Maan || 2210991833 - Kushagra Grover || 2210991829 - Kunwardeep Nagpal </p>
+</section>`;
+}
+export default exportF2;
